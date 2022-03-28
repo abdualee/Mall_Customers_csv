8c4a2e1edcba618374c8e8672dedc85fd66ef87b
abdul.aleem@mdlz.com <abdul.aleem@mdlz.com>
2022-03-28 23:34:09 +0000

    d

---
+++ 2022-03-24 - DBFS Example (1).py
@@ -0,0 +1,102 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Overview
+# MAGIC 
+# MAGIC This notebook will show you how to create and query a table or DataFrame that you uploaded to DBFS. [DBFS](https://docs.databricks.com/user-guide/dbfs-databricks-file-system.html) is a Databricks File System that allows you to store data for querying inside of Databricks. This notebook assumes that you have a file already inside of DBFS that you would like to read from.
+# MAGIC 
+# MAGIC This notebook is written in **Python** so the default cell type is Python. However, you can use different languages by using the `%LANGUAGE` syntax. Python, Scala, SQL, and R are all supported.
+
+# COMMAND ----------
+
+# File location and type
+file_location = "/FileStore/tables/Mall_Customers.csv"
+file_type = "csv"
+
+# CSV options
+infer_schema = "true"
+first_row_is_header = "true"
+delimiter = ","
+
+# The applied options are for CSV files. For other file types, these will be ignored.
+df = spark.read.format(file_type) \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load(file_location)
+
+display(df)
+
+# COMMAND ----------
+
+# Create a view or table
+
+temp_table_name = "Mall_Customers_csv"
+
+df.createOrReplaceTempView(temp_table_name)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC /* Query the created temp table in a SQL cell */
+# MAGIC 
+# MAGIC select * from `Mall_Customers_csv`
+
+# COMMAND ----------
+
+# With this registered as a temp view, it will only be available to this particular notebook. If you'd like other users to be able to query this table, you can also create a table from the DataFrame.
+# Once saved, this table will persist across cluster restarts as well as allow various users across different notebooks to query this data.
+# To do so, choose your table name and uncomment the bottom line.
+
+permanent_table_name = "Mall_Customers_csv"
+
+# df.write.format("parquet").saveAsTable(permanent_table_name)
+
+# COMMAND ----------
+
+df_customermall=df.toPandas()
+
+# COMMAND ----------
+
+import pandas as pd
+
+# COMMAND ----------
+
+df_customermall.dtypes
+
+# COMMAND ----------
+
+df_x=df_customermall.iloc[:,3:]
+
+# COMMAND ----------
+
+df_x.head()
+
+# COMMAND ----------
+
+from sklearn.preprocessing import StandardScaler
+
+# COMMAND ----------
+
+df_mod=StandardScaler().fit_transform(df_x)
+
+# COMMAND ----------
+
+df_mod
+
+# COMMAND ----------
+
+from sklearn.cluster import KMeans
+distance=[]
+for i in range (1,11):
+    mod=KMeans(n_clusters=i, init="random").fit(df_mod)
+    distance.append(mod.inertia_)
+
+# COMMAND ----------
+
+import matplotlib.pyplot as plt
+plt.plot(range(1,11),distance)
+
+# COMMAND ----------
+
+# MAGIC %sh pwd
